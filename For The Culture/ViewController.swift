670eabff8323f7cb0f0d9ec3a06c73062d156619
Olive B <oliveb@Olives-MacBook-Pro.local>
2018-09-18 12:20:48 +0100

    Final FTC

---
+++ For The Culture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  For The Culture
+//
+//  Created by Olive B on 18/09/2018.
+//  Copyright © 2018 Olive B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
